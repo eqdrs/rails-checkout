7223203d5276b011a67398f0a06f548b72d1e48f
Everton e Marcel <par07@campuscode.com.br>
2019-01-30 15:48:57 -0200

    Vendedor realiza login

--- spec/features/vendor_sign_in_spec.rb
+++ spec/features/vendor_sign_in_spec.rb
@@ -15,4 +15,16 @@ feature 'Vendor sign in' do
     expect(page).to have_link 'Sair'
     expect(page).to have_content "Olá, #{vendor.email}"
   end
+
+  scenario 'and cant sign in with invalid email' do
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: 'invalido@teste.com'
+    fill_in 'Senha', with: '12345678'
+    click_on 'Entrar'
+
+    expect(current_path).to eq new_user_session_path
+    expect(page).to have_content "Email ou senha inválida."
+  end
 end
