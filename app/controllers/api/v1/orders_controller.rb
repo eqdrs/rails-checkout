8a3df14ff458bbb04b7ebaf84cb25999e3dca4e8
kamyla e jader <par06@campuscode.com.br>
2019-02-05 15:54:37 -0200

    Cria api retorno de pedidos Resolve rubocop

---
+++ app/controllers/api/v1/orders_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::OrdersController < Api::V1::ApplicationController
+  def index
+    render json: Order.where(status: :approved)
+  end
+
+  private
+
+  def error?
+    if @order.errors.any?
+      render status: :precondition_failed,
+             json: { message: @order.errors.full_messages }
+    else
+      render json: { message: '' }
+    end
+  end
+end
