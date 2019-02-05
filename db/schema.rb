cb5db2e192fc8052feaf5f62d8e5fb76b3664673
Erick e Jefferson <par05@campuscode.com.br>
2019-02-05 14:15:04 -0200

    Correção de conflitos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_04_180411) do
+ActiveRecord::Schema.define(version: 2019_02_04_194304) do
 
   create_table "cancelled_orders", force: :cascade do |t|
     t.text "internal_reason"
@@ -45,8 +45,8 @@ ActiveRecord::Schema.define(version: 2019_02_04_180411) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "customer_id"
     t.integer "user_id"
+    t.integer "customer_id"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
