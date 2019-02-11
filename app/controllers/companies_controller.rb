b2c12009b3ad5363426f88940029f674ecabaa7f
Kamyla e Erick <kamyla.aragao55@gmail.com>
2019-02-11 20:50:41 -0200

    busca por cnpj - corrigindo teste falso positivo e respectivo bug

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -19,10 +19,10 @@ class CompaniesController < ApplicationController
   end
 
   def search
-    @company = Company.find_by(cnpj: params[:cnpj])
+    @company = Company.find_by(cnpj: CNPJ.new(params[:cnpj]).stripped)
     if @company.nil?
       redirect_to(search_customer_path,
-                  alert: I18n.t('individuals.search.not_found'))
+                  alert: I18n.t('companies.search.not_found'))
     else
       redirect_to @company
     end
