8a04348627a851cabc971577da9117ffbefdaeb3
Jefferson Barbosa <jefferson.dab@gmail.com>
2019-02-11 19:58:28 -0200

    WIP: Admin gerencia usuários (teste de request 3/3)

--- spec/requests/user_manages_vendors_spec.rb
+++ spec/requests/user_manages_vendors_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Direct requests to deactivate vendors' do
+  it 'successfully if current user is an admin' do
+    admin = create(:user)
+    login_as admin
+    vendor = create(:vendor)
+
+    post "/users/#{vendor.id}/deactivate"
+    vendor.reload
+    follow_redirect!
+
+    expect(response.body).to include(I18n.t('users.messages.inactive_user',
+                                       email: vendor.email))
+    expect(vendor).not_to be_active
+  end
+
   it 'should fail when user is not logged in' do
     user = create(:vendor)
     post "/users/#{user.id}/deactivate"
