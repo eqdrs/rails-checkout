f1569a7cd7670d65df2016d996e9c3170fe823d4
Everton Quadros <evertonquadros15@gmail.com>
2019-02-12 02:26:57 -0200

    Pequenos ajustes na página de exibição de clientes

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,11 @@
 product = Product.create(name: 'Host', price: 24.0)
 other_product = Product.create(name: 'Host2', price: 30.0)
 Product.create(name: 'Host3', price: 45.0)
-customer = Individual.create(name: 'Kamyla Aragão', address: 'rua X', cpf: '28813510420', phone: '1234534', email: 'email@email.com.br')
 
-vendor = User.create(email: 'vendor@vendas.com', password: '12345678', role: :vendor)
-User.create(email: 'admin@vendas.com', password: '12345678', role: :admin)
+vendor = User.create!(email: 'vendor@vendas.com', password: '12345678', role: :vendor)
+user = User.create!(email: 'admin@vendas.com', password: '12345678', role: :admin)
+
+customer = Individual.create!(name: 'Kamyla Aragão', address: 'rua X', cpf: '28813510420', phone: '(11) 9345-2345', email: 'kamyla@email.com.br', user: vendor)
 
 Order.create!(product: product, status: :open, customer: customer, user: vendor)
-Order.create(product: other_product, status: :approved, sent_to_client_app: :sent, customer: customer, user: vendor)
+Order.create!(product: other_product, status: :approved, sent_to_client_app: :sent, customer: customer, user: vendor)
